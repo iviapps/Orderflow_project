85fdd27c3e9f59571a3fe901e454ef9bd591bea5
iviapps <iviapps.content@gmail.com>
2025-11-11 15:44:23 +0100

    learning first day

--- Overflow/Overflow.AppHost/AppHost.cs
+++ Overflow/Overflow.AppHost/AppHost.cs
@@ -1,3 +1,19 @@
+using Aspire.Hosting;
+using Aspire.Hosting.PostgreSQL; // habilita AddPostgres y AddDatabase
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+// 1) Servidor Postgres con volumen de datos (persistencia real)
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume();
+
+
+//en el castor pone que no existe 
+// 2) Base de datos específica para tu servicio
+var identityDb = postgres.AddDatabase("identitydb");
+
+// 3) Tu servicio .NET que usa esa base de datos
+builder.AddProject<Projects.Overflow_Identity>("overflow-identity")
+    .WithReference(identityDb); // mejor que referenciar el servidor
 
 builder.Build().Run();
